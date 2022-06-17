2416229a00647f525ed211e3e6d14f56f42f94f4
Tom Reitz <tom@tomreitz.com>
2022-06-17 17:06:55 -0500

    Update __init__.py

--- ea_airflow_util/__init__.py
+++ ea_airflow_util/__init__.py
@@ -1,2 +1,3 @@
 from ea_airflow_util.dags.aws_param_store_to_airflow_dag import AWSParamStoreToAirflowDAG
 from ea_airflow_util.dags.run_dbt_airflow_dag import RunDbtDag
+from ea_airflow_util.dags.update_dbt_docs_dag import UpdateDbtDocsDag
