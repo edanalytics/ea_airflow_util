927f61b5ffbcb208606e9c7594a1eae1b1a6b878
jkaiser <jayckaiser@gmail.com>
2022-11-22 13:34:06 -0600

    Clean up ParamStore to Airflow DAG and prepare for testing.

--- ea_airflow_util/dags/aws_param_store_to_airflow_dag.py
+++ ea_airflow_util/dags/aws_param_store_to_airflow_dag.py
@@ -12,13 +12,21 @@ class AWSParamStoreToAirflowDAG:
     """
 
     """
-    def __init__(self, ssm_prefix: str, s3_region: str, **kwargs):
+    def __init__(self,
+        ssm_prefix: str,
+        region_name: str = None,
+        **kwargs
+    ):
         self.ssm_prefix = ssm_prefix
-        self.s3_region = s3_region
+        self.region_name = region_name
         self.dag = self.build_dag(**kwargs)
 
 
-    def build_dag(self, dag_id: str, default_args: dict, **kwargs):
+    def build_dag(self,
+        dag_id: str,
+        default_args: dict,
+        **kwargs
+    ):
         """
 
         :param dag_id:
@@ -31,10 +39,11 @@ class AWSParamStoreToAirflowDAG:
             """
             :return:
             """
-            param_store = SSMParameterStore(prefix=self.ssm_prefix, region_name=self.s3_region)
+            param_store = SSMParameterStore(prefix=self.ssm_prefix, region_name=self.region_name)
 
-            for param_secret in param_store.values():
-                self.create_conn(**param_secret)
+            for param_name, param_secret in param_store.items():
+                conn_id = param_name.replace(self.ssm_prefix, "")
+                self.create_conn(conn_id=conn_id, **param_secret)
 
 
         # This syntax ensures param_store stays hidden within the class.
@@ -52,15 +61,15 @@ class AWSParamStoreToAirflowDAG:
     # stackoverflow link:
     # https://stackoverflow.com/questions/51863881/is-there-a-way-to-create-modify-connections-through-airflow-api
     @staticmethod
-    def create_conn(**kwargs) -> Connection:
+    def create_conn(conn_id: str, **kwargs) -> Connection:
         """
         Store a new connection in Airflow Meta DB
 
+        :param conn_id:
         :param kwargs:
         :return:
 
         :Keyword Arguments:
-            * conn_id
             * conn_type
             * host
             * schema
@@ -69,28 +78,23 @@ class AWSParamStoreToAirflowDAG:
             * port
             * extra
         """
-        conn = Connection(**kwargs)
-
+        conn = Connection(conn_id=conn_id, **kwargs)
         session = airflow.settings.Session()
-        conn_name = (
-            session
-                .query(Connection)
-                .filter(Connection.conn_id == conn.conn_id)
-                .first()
-        )
 
-        if str(conn_name) == str(conn.conn_id):
+        # Verify whether the connection already exists in Airflow.
+        if session.query(Connection).filter(Connection.conn_id == conn.conn_id).first():
             logging.warning(
                 f"Connection {conn.conn_id} already exists!"
             )
             return None
 
-        session.add(conn)
-        session.commit()
+        else:
+            session.add(conn)
+            session.commit()
 
-        logging.info(Connection.log_info(conn))
-        logging.info(
-            f"Connection {conn_id} was added."
-        )
+            logging.info(Connection.log_info(conn))
+            logging.info(
+                f"Connection {conn_id} was added."
+            )
 
-        return conn
+            return conn
