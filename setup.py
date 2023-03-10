cf251daec0c79ef82a5b7520ef82bc12e8b58a8a
ejoranlienea <ejoranlien@edanalytics.org>
2023-03-10 14:56:22 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
       name='ea_airflow_util',
-      version='0.1',
+      version='0.1.1',
       description='EA Airflow tools',
       url='',
       author='Jay Kaiser',
