78c600e327616dbf6cba0110fc3fa19b0fc15421
jkaiser <jayckaiser@gmail.com>
2023-05-19 16:18:02 -0500

    Add new operators for checking and updating Airflow variables.

--- ea_airflow_util/__init__.py
+++ ea_airflow_util/__init__.py
@@ -3,3 +3,4 @@ from ea_airflow_util.dags.run_dbt_airflow_dag import RunDbtDag
 from ea_airflow_util.dags.update_dbt_docs_dag import UpdateDbtDocsDag
 
 import ea_airflow_util.dags.dag_util.slack_callbacks as slack_callbacks
+from ea_airflow_util.dags.operators.variable import build_variable_check_operator, build_variable_update_operator
