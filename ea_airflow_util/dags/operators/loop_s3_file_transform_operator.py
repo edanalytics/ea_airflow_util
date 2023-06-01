aa568528cfd27c341686e7ab97bbbac8a95fd26f
rlittle08 <rlittle@edanalytics.org>
2023-06-01 12:50:46 -0500

    initial draft of s3 snowflake dag

---
+++ ea_airflow_util/dags/operators/loop_s3_file_transform_operator.py
@@ -0,0 +1,171 @@
+from tempfile import NamedTemporaryFile
+import subprocess
+import sys
+import os
+import pathlib
+
+from airflow.exceptions import AirflowException, AirflowSkipException
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoopS3FileTransformOperator(BaseOperator):
+    template_fields = ('source_s3_keys', 'dest_s3_prefix', 'transform_script')
+    """
+    Copies data from a source S3 location to a temporary location on the
+    local filesystem. Runs a transformation on this file as specified by
+    the transformation script and uploads the output to a destination S3
+    location.
+
+    The locations of the source and the destination files in the local
+    filesystem is provided as an first and second arguments to the
+    transformation script. The transformation script is expected to read the
+    data from source, transform it and write the output to the local
+    destination file. The operator then takes over control and uploads the
+    local destination file to S3.
+
+    S3 Select is also available to filter the source contents. Users can
+    omit the transformation script if S3 Select expression is specified.
+
+    :param source_s3_keys: The list of keys to be retrieved from S3. (templated)
+    :type source_s3_key: list
+    :param source_aws_conn_id: source s3 connection
+    :type source_aws_conn_id: str
+    :param source_verify: Whether or not to verify SSL certificates for S3 connection.
+        By default SSL certificates are verified.
+        You can provide the following values:
+
+        - ``False``: do not validate SSL certificates. SSL will still be used
+             (unless use_ssl is False), but SSL certificates will not be
+             verified.
+        - ``path/to/cert/bundle.pem``: A filename of the CA cert bundle to uses.
+             You can specify this argument if you want to use a different
+             CA cert bundle than the one used by botocore.
+
+        This is also applicable to ``dest_verify``.
+    :type source_verify: bool or str
+    :param dest_aws_conn_id: destination s3 connection
+    :type dest_aws_conn_id: str
+    :param replace: Replace dest S3 key if it already exists
+    :type replace: bool
+    :param transform_script: location of the executable transformation script
+    :type transform_script: str
+    :param select_expression: S3 Select expression
+    :type select_expression: str
+    """
+
+
+    @apply_defaults
+    def __init__(
+            self,
+            source_s3_keys = [],
+            dest_s3_prefix = '',
+            dest_s3_file_extension=None,
+            transform_script=None,
+            select_expression=None,
+            source_aws_conn_id='aws_default',
+            source_verify=None,
+            dest_aws_conn_id='aws_default',
+            dest_verify=None,
+            replace=False,
+            *args, **kwargs):
+        super(LoopS3FileTransformOperator, self).__init__(*args, **kwargs)
+        self.source_s3_keys = source_s3_keys
+        self.dest_s3_prefix = dest_s3_prefix
+        self.dest_s3_file_extension = dest_s3_file_extension
+        self.source_aws_conn_id = source_aws_conn_id
+        self.source_verify = source_verify
+        self.dest_aws_conn_id = dest_aws_conn_id
+        self.dest_verify = dest_verify
+        self.replace = replace
+        self.transform_script = transform_script
+        self.select_expression = select_expression
+        self.output_encoding = sys.getdefaultencoding()
+
+    def execute(self, context):
+        if self.transform_script is None and self.select_expression is None:
+            raise AirflowException(
+                "Either transform_script or select_expression must be specified")
+
+        if not self.source_s3_keys:
+            raise AirflowSkipException(
+                "No files found in source S3 bucket to transfer"
+            )
+
+        source_s3 = S3Hook(aws_conn_id=self.source_aws_conn_id,
+                           verify=self.source_verify)
+        dest_s3 = S3Hook(aws_conn_id=self.dest_aws_conn_id,
+                         verify=self.dest_verify)
+
+        transferred_keys = []
+
+        for source_s3_key in self.source_s3_keys:
+
+            self.log.info("Downloading source S3 file %s", source_s3_key)
+            if not source_s3.check_for_key(source_s3_key):
+                # raise AirflowException(
+                #     "The source key {0} does not exist".format(source_s3_key))
+                self.log.info(f"{source_s3_key} does not exist")
+                continue
+            source_s3_key_object = source_s3.get_key(source_s3_key)
+
+            with NamedTemporaryFile("wb") as f_source, NamedTemporaryFile("wb") as f_dest:
+                self.log.info(
+                    "Dumping S3 file %s contents to local file %s",
+                    source_s3_key, f_source.name
+                )
+
+                if self.select_expression is not None:
+                    content = source_s3.select_key(
+                        key=source_s3_key,
+                        expression=self.select_expression
+                    )
+                    f_source.write(content.encode("utf-8"))
+                else:
+                    source_s3_key_object.download_fileobj(Fileobj=f_source)
+                f_source.flush()
+
+                if self.transform_script is not None:
+                    process = subprocess.Popen(
+                        [self.transform_script, f_source.name, f_dest.name],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        close_fds=True
+                    )
+
+                    self.log.info("Output:")
+                    for line in iter(process.stdout.readline, b''):
+                        self.log.info(line.decode(self.output_encoding).rstrip())
+
+                    process.wait()
+
+                    if process.returncode > 0:
+                        raise AirflowException(
+                            "Transform script failed: {0}".format(process.returncode)
+                        )
+                    else:
+                        self.log.info(
+                            "Transform script successful. Output temporarily located at %s",
+                            f_dest.name
+                        )
+
+
+                source_file_extension = pathlib.Path(source_s3_key).suffix
+                if not self.dest_s3_file_extension:
+                    self.dest_s3_file_extension = source_file_extension
+                dest_s3_key = os.path.join(self.dest_s3_prefix,
+                                           source_s3_key.replace(source_file_extension,self.dest_s3_file_extension).split('/')[-1]
+                                           )
+
+                self.log.info("Uploading transformed file to S3")
+                f_dest.flush()
+                dest_s3.load_file(
+                    filename=f_dest.name,
+                    key=dest_s3_key,
+                    replace=self.replace
+                )
+                self.log.info("Upload successful")
+
+                transferred_keys.append(dest_s3_key)
+        
+        return transferred_keys
---
+++ ea_airflow_util/dags/s3_to_snowflake_dag.py
@@ -0,0 +1,251 @@
+from datetime import datetime
+import logging
+
+from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.helpers import chain
+from airflow.decorators import dag, task
+from airflow.exceptions import AirflowSkipException
+from edu_edfi_airflow.dags.dag_util.airflow_util import xcom_pull_template
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator, S3FileTransformOperator
+
+from operators.loop_s3_file_transform_operator import LoopS3FileTransformOperator
+
+from airflow import DAG
+
+from util import io_helpers
+import os
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+# Load variables from Airflow config
+configs_dir = '/home/airflow/airflow/configs'
+airflow_configs_file = 'airflow_config.yml'
+airflow_configs = io_helpers.safe_load_yaml(configs_dir, airflow_configs_file)
+
+
+dag_params = airflow_configs.get('s3_to_snowflake_dags')
+if dag_params is None:
+    raise Exception(
+        "Necessary parameter `s3_to_snowflake_dags` is missing from the configs file!"
+    )
+
+class S3ToSnowflakeDag:
+    """
+    This DAG transfers data from an S3 bucket location into the Snowflake raw data lake. It should be used when data sources
+    are not available from an Ed-Fi ODS but need to be brought into the data warehouse.
+
+    """
+    def __init__(self,
+        *,
+        tenant_code: str,
+        api_year: int,
+
+        database: str,
+        schema: str,
+        data_source: str,
+        resource_names: str,
+
+        s3_source_conn_id : str,
+        s3_dest_conn_id : str,
+        transform_script: str,
+        slack_conn_id : str,
+        snowflake_conn_id : str,
+        pool     : str,
+
+        **kwargs
+    ) -> None:
+        self.tenant_code = tenant_code
+        self.api_year = api_year
+
+        self.database = database
+        self.schema = schema
+        self.data_source = data_source
+        self.resource_names = resource_names
+        
+        self.s3_source_conn_id = s3_source_conn_id
+        self.s3_dest_conn_id = s3_dest_conn_id
+        self.transform_script = transform_script
+        self.snowflake_conn_id = snowflake_conn_id
+        self.slack_conn_id = slack_conn_id
+        self.pool = pool
+
+        self.dag = self.initialize_dag(**kwargs)
+
+
+    def initialize_dag(self,
+        dag_id: str,
+        schedule_interval: str,
+        default_args: dict,
+        **kwargs
+    ) -> DAG:
+        """
+
+        :param dag_id:
+        :param schedule_interval:
+        :param default_args:
+        :return:
+        """
+        # If a Slack connection has been defined, add the failure callback to the default_args.
+        if self.slack_conn_id:
+            slack_failure_callback = partial(slack_callbacks.slack_alert_failure, http_conn_id=self.slack_conn_id)
+            default_args['on_failure_callback'] = slack_failure_callback
+
+            # Define an SLA-miss callback as well.
+            slack_sla_miss_callback = partial(slack_callbacks.slack_alert_sla_miss, http_conn_id=self.slack_conn_id)
+        else:
+            slack_sla_miss_callback = None
+
+        # If a Slack connection is defined, send a callback in the event of a DAG failure.
+        return DAG(
+            dag_id=dag_id,
+            schedule_interval=schedule_interval,
+            default_args=default_args,
+            catchup=False,
+            render_template_as_native_obj=True,
+            max_active_runs=1,
+            sla_miss_callback=slack_sla_miss_callback,
+        )
+
+    def copy_from_datalake_to_raw(self, resource_name, datalake_prefix):
+        """
+        Copy raw data from data lake to data warehouse, including object metadata.
+        """
+        
+        logging.info(f"Copying from data lake to raw: {datalake_prefix}/{resource_name}")
+        # TODO: should we have FORCE=TRUE? this is useful if data have been deleted from raw & want to re-load
+        # if so, add this line `on_error='continue', FORCE = TRUE`
+        sql = f'''
+            copy into {self.database}.{self.schema}.{self.data_source}_{resource_name} (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, v)
+            from (
+                select
+                    split_part(metadata$filename, '/', 1) as tenant_code,
+                    split_part(metadata$filename, '/', 2) as api_year,
+                    to_date(split_part(metadata$filename, '/', 3), 'YYYYMMDD') as pull_date,
+                    to_timestamp(split_part(metadata$filename, '/', 4), 'YYYYMMDDTHH24MISS') as pull_timestamp,
+                    metadata$file_row_number as file_row_number,
+                    metadata$filename as filename,
+                    '{resource_name}' as name,
+                    t.$1 as v
+                from @{self.database}.util.airflow_stage/{datalake_prefix}/{resource_name}/
+                (file_format => 'json_default') t
+            )
+        '''
+            
+        copy_to_snowflake_raw = SnowflakeOperator(
+        task_id=f'copy_to_snowflake_raw',
+        snowflake_conn_id=self.snowflake_conn_id,
+        sql=sql
+        )
+        #todo ask jay if this operator within operator makes sense?
+        #todo how to get log to show actual rows copied? right now it says "1 row affected" no matter what
+        copy_to_snowflake_raw.execute(dict())
+
+    def delete_from_source(self, s3_source_hook, s3_source_bucket, s3_source_keys):
+        """
+        Delete the object from the source bucket.
+        """
+        logging.info('Deleting file from source s3')
+        s3_source_hook.delete_objects(bucket=s3_source_bucket, keys=s3_source_keys)
+
+    def build_s3_to_snowflake_dag(self,
+                                  **kwargs):
+
+        # todo: figure out what will happen if we run this dag twice in one day. will it break depending on the drop/replace logic?
+        # datalake_prefix = os.path.join(self.tenant_code, 
+        #                                str(self.api_year), 
+        #                                '{{ ds_nodash }}',
+        #                                '{{ ts_nodash }}')
+ 
+        for resource_name in self.resource_names:
+
+            s3_source_prefix = os.path.join(self.data_source,
+                                            str(self.api_year),
+                                            '{{ ds_nodash }}',
+                                            resource_name)
+            s3_source_hook = S3Hook(aws_conn_id=self.s3_source_conn_id)
+            s3_source_bucket = s3_source_hook.get_connection(self.s3_source_conn_id).schema   
+
+            datalake_prefix = os.path.join(self.tenant_code, 
+                                        str(self.api_year), 
+                                        '{{ ds_nodash }}',
+                                        resource_name)
+            
+            list_s3_objects = S3ListOperator(
+                task_id=f'list_s3_objects_{resource_name}',
+                bucket=s3_source_bucket,
+                prefix=s3_source_prefix,
+                delimiter='',
+                aws_conn_id=self.s3_source_conn_id,
+                dag=self.dag
+            )
+
+            if self.s3_dest_conn_id:
+                transfer_s3_to_s3 = LoopS3FileTransformOperator(
+                    task_id=f'transfer_s3_to_s3_{resource_name}',
+                    source_s3_keys= xcom_pull_template(f'list_s3_objects_{resource_name}'),
+                    dest_s3_prefix=datalake_prefix,
+                    transform_script=self.transform_script,
+                    source_aws_conn_id=self.s3_source_conn_id,
+                    dest_aws_conn_id=self.s3_dest_conn_id,
+                    # TODO: should this always be true?
+                    replace=True,
+                    dag=self.dag)
+            else:
+                transfer_s3_to_s3 = None
+
+            copy_to_raw = PythonOperator(
+                task_id=f'copy_to_raw_{resource_name}',
+                python_callable=self.copy_from_datalake_to_raw,
+                op_kwargs={
+                    'resource_name': resource_name,
+                    'datalake_prefix': datalake_prefix
+                },
+                dag=self.dag
+            )
+
+            # only delete from source if we transferred data to a different bucket
+            if self.s3_dest_conn_id:
+                delete_from_source = PythonOperator(
+                    task_id=f'delete_from_source_{resource_name}',
+                    python_callable=self.delete_from_source,
+                    op_kwargs={
+                        's3_source_hook': s3_source_hook,
+                        's3_source_bucket': s3_source_bucket,
+                        's3_source_keys': xcom_pull_template(f'list_s3_objects_{resource_name}')
+                    },
+                    dag=self.dag
+                )
+            else:
+                delete_from_source = None
+
+            ### Default route: List -> Transfer -> Copy -> Delete from source
+            task_order = (
+                list_s3_objects,
+                transfer_s3_to_s3,
+                copy_to_raw,
+                delete_from_source
+            )
+
+            chain(*filter(None, task_order))  # Chain all defined operators into task-order.
+
+for tenant_code, data_sources in dag_params.items():
+    
+    for data_source, api_year_vars in data_sources.items():
+
+        for api_year, dag_vars in api_year_vars.items():
+
+            s3_to_snowflake_dag_id = f"s3_to_snowflake_{tenant_code}_{data_source}_{api_year}"
+
+            s3_to_snowflake_dag = S3ToSnowflakeDag(
+                    dag_id=s3_to_snowflake_dag_id,
+                    tenant_code=tenant_code,
+                    api_year=api_year,
+                    data_source=data_source,
+                    **dag_vars
+                )
+            
+            s3_to_snowflake_dag.build_s3_to_snowflake_dag()
+
+            globals()[s3_to_snowflake_dag.dag.dag_id] = s3_to_snowflake_dag.dag
