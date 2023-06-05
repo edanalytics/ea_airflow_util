2c592e82a94af0b0e1d359f5644fae128d7d4e3d
jkaiser <jayckaiser@gmail.com>
2023-06-05 14:02:20 -0500

    Clean up S3ToSnowflakeDAG.

--- ea_airflow_util/dags/s3_to_snowflake_dag.py
+++ ea_airflow_util/dags/s3_to_snowflake_dag.py
@@ -1,19 +1,19 @@
+import os
 import logging
+
 from functools import partial
 
+from airflow import DAG
 from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.utils.helpers import chain
-from .dag_util.xcom_util import xcom_pull_template
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
+from airflow.utils.helpers import chain
 
 import ea_airflow_util.dags.dag_util.slack_callbacks as slack_callbacks
 from .operators.loop_s3_file_transform_operator import LoopS3FileTransformOperator
+from .dag_util.xcom_util import xcom_pull_template
 
-from airflow import DAG
-
-import os
 
 class S3ToSnowflakeDag():
     """
@@ -26,32 +26,36 @@ class S3ToSnowflakeDag():
         tenant_code: str,
         api_year: int,
 
+        snowflake_conn_id: str,
         database: str,
         schema: str,
+
         data_source: str,
         resource_names: str,
-
-        s3_source_conn_id : str,
-        s3_dest_conn_id : str,
         transform_script: str,
-        slack_conn_id : str,
-        snowflake_conn_id : str,
-        pool     : str,
+
+        s3_source_conn_id: str,
+        s3_dest_conn_id: str,
+
+        slack_conn_id: str,
+        pool: str,
 
         **kwargs
     ) -> None:
         self.tenant_code = tenant_code
         self.api_year = api_year
 
-        self.data_source = data_source
-        self.resource_names = resource_names
-        
-        self.s3_source_conn_id = s3_source_conn_id
-        self.s3_dest_conn_id = s3_dest_conn_id
-        self.transform_script = transform_script
         self.snowflake_conn_id = snowflake_conn_id
         self.database = database
         self.schema = schema
+
+        self.data_source = data_source
+        self.resource_names = resource_names
+        self.transform_script = transform_script
+
+        self.s3_source_conn_id = s3_source_conn_id
+        self.s3_dest_conn_id = s3_dest_conn_id
+
         self.slack_conn_id = slack_conn_id
         self.pool = pool
 
@@ -89,51 +93,10 @@ class S3ToSnowflakeDag():
             render_template_as_native_obj=True,
             max_active_runs=1,
             sla_miss_callback=slack_sla_miss_callback,
+            **kwargs
         )
 
-    def copy_from_datalake_to_raw(self, resource_name, datalake_prefix):
-        """
-        Copy raw data from data lake to data warehouse, including object metadata.
-        """
-        
-        logging.info(f"Copying from data lake to raw: {datalake_prefix}/{resource_name}")
-        # TODO: should we have FORCE=TRUE? this is useful if data have been deleted from raw & want to re-load
-        # if so, add this line `on_error='continue', FORCE = TRUE`
-        sql = f'''
-            copy into {self.database}.{self.schema}.{self.data_source}__{resource_name} (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, v)
-            from (
-                select
-                    split_part(metadata$filename, '/', 1) as tenant_code,
-                    split_part(metadata$filename, '/', 2) as api_year,
-                    to_date(split_part(metadata$filename, '/', 3), 'YYYYMMDD') as pull_date,
-                    to_timestamp(split_part(metadata$filename, '/', 4), 'YYYYMMDDTHH24MISS') as pull_timestamp,
-                    metadata$file_row_number as file_row_number,
-                    metadata$filename as filename,
-                    '{resource_name}' as name,
-                    t.$1 as v
-                from @{self.database}.util.airflow_stage/{datalake_prefix}/{resource_name}/
-                (file_format => 'json_default') t
-            )
-        '''
-            
-        copy_to_snowflake_raw = SnowflakeOperator(
-        task_id=f'copy_to_snowflake_raw',
-        snowflake_conn_id=self.snowflake_conn_id,
-        sql=sql
-        )
-        #todo ask jay if this operator within operator makes sense?
-        #todo how to get log to show actual rows copied? right now it says "1 row affected" no matter what
-        copy_to_snowflake_raw.execute(dict())
-
-    def delete_from_source(self, s3_source_hook, s3_source_bucket, s3_source_keys):
-        """
-        Delete the object from the source bucket.
-        """
-        logging.info('Deleting file from source s3')
-        s3_source_hook.delete_objects(bucket=s3_source_bucket, keys=s3_source_keys)
-
-    def build_s3_to_snowflake_dag(self,
-                                  **kwargs):
+    def build_s3_to_snowflake_dag(self, **kwargs):
 
         # todo: figure out what will happen if we run this dag twice in one day. will it break depending on the drop/replace logic?
         # datalake_prefix = os.path.join(self.tenant_code, 
@@ -143,18 +106,17 @@ class S3ToSnowflakeDag():
  
         for resource_name in self.resource_names:
 
-            s3_source_prefix = os.path.join(self.tenant_code,
-                                            str(self.api_year),
-                                            self.data_source,
-                                            '{{ ds_nodash }}',
-                                            resource_name)
+            s3_source_prefix = os.path.join(
+                self.tenant_code, str(self.api_year),
+                self.data_source, '{{ ds_nodash }}', resource_name
+            )
             s3_source_hook = S3Hook(aws_conn_id=self.s3_source_conn_id)
             s3_source_bucket = s3_source_hook.get_connection(self.s3_source_conn_id).schema   
 
-            datalake_prefix = os.path.join(self.tenant_code, 
-                                        str(self.api_year), 
-                                        '{{ ds_nodash }}',
-                                        resource_name)
+            datalake_prefix = os.path.join(
+                self.tenant_code, str(self.api_year),
+                '{{ ds_nodash }}', resource_name
+            )
             
             list_s3_objects = S3ListOperator(
                 task_id=f'list_s3_objects_{resource_name}',
@@ -168,7 +130,7 @@ class S3ToSnowflakeDag():
             if self.s3_dest_conn_id:
                 transfer_s3_to_s3 = LoopS3FileTransformOperator(
                     task_id=f'transfer_s3_to_s3_{resource_name}',
-                    source_s3_keys= xcom_pull_template(f'list_s3_objects_{resource_name}'),
+                    source_s3_keys=xcom_pull_template(list_s3_objects.task_id),
                     dest_s3_prefix=datalake_prefix,
                     transform_script=self.transform_script,
                     source_aws_conn_id=self.s3_source_conn_id,
@@ -176,7 +138,8 @@ class S3ToSnowflakeDag():
                     # TODO: should this always be true? if false, and you are running an identically named file to a previous run (in same directory), the new file will be ignored
                     # this most commonly impacts testing, will rarely occur in real time, unless you do mulitple dag runs per day?
                     replace=True,
-                    dag=self.dag)
+                    dag=self.dag
+                )
             else:
                 transfer_s3_to_s3 = None
 
@@ -196,9 +159,9 @@ class S3ToSnowflakeDag():
                     task_id=f'delete_from_source_{resource_name}',
                     python_callable=self.delete_from_source,
                     op_kwargs={
-                        's3_source_hook': s3_source_hook,
+                        's3_source_hook'  : s3_source_hook,
                         's3_source_bucket': s3_source_bucket,
-                        's3_source_keys': xcom_pull_template(f'list_s3_objects_{resource_name}')
+                        's3_source_keys'  : xcom_pull_template(list_s3_objects.task_id)
                     },
                     dag=self.dag
                 )
@@ -214,3 +177,48 @@ class S3ToSnowflakeDag():
             )
 
             chain(*filter(None, task_order))  # Chain all defined operators into task-order.
+
+    def copy_from_datalake_to_raw(self, resource_name, datalake_prefix):
+        """
+        Copy raw data from data lake to data warehouse, including object metadata.
+        """
+
+        logging.info(f"Copying from data lake to raw: {datalake_prefix}/{resource_name}")
+        # TODO: should we have FORCE=TRUE? this is useful if data have been deleted from raw & want to re-load
+        # if so, add this line `on_error='continue', FORCE = TRUE`
+        sql = f'''
+            copy into {self.database}.{self.schema}.{self.data_source}__{resource_name}
+                (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, v)
+            from (
+                select
+                    split_part(metadata$filename, '/', 1) as tenant_code,
+                    split_part(metadata$filename, '/', 2) as api_year,
+                    to_date(split_part(metadata$filename, '/', 3), 'YYYYMMDD') as pull_date,
+                    to_timestamp(split_part(metadata$filename, '/', 4), 'YYYYMMDDTHH24MISS') as pull_timestamp,
+                    metadata$file_row_number as file_row_number,
+                    metadata$filename as filename,
+                    '{resource_name}' as name,
+                    t.$1 as v
+                from @{self.database}.util.airflow_stage/{datalake_prefix}/{resource_name}/
+                (file_format => 'json_default') t
+            )
+        '''
+
+        copy_to_snowflake_raw = SnowflakeOperator(
+            task_id=f'copy_to_snowflake_raw',
+            snowflake_conn_id=self.snowflake_conn_id,
+            sql=sql
+        )
+        # todo ask jay if this operator within operator makes sense?
+        #   TODO: @Robbie it does not.
+        # todo how to get log to show actual rows copied? right now it says "1 row affected" no matter what
+        copy_to_snowflake_raw.execute(dict())
+
+
+    @staticmethod
+    def delete_from_source(s3_source_hook, s3_source_bucket, s3_source_keys):
+        """
+        Delete the object from the source bucket.
+        """
+        logging.info('Deleting file from source s3')
+        s3_source_hook.delete_objects(bucket=s3_source_bucket, keys=s3_source_keys)
