bff73fbe5d2e1aece0f3ee5ea8cbcd11164b1cd4
jkaiser <jayckaiser@gmail.com>
2023-06-09 13:08:52 -0500

    Refactor LoopS3FileTransformOperator to take advantage of S3FileTransformOperator's execute, instead of rebuilding it.

--- ea_airflow_util/dags/operators/loop_s3_file_transform_operator.py
+++ ea_airflow_util/dags/operators/loop_s3_file_transform_operator.py
@@ -1,72 +1,30 @@
-import logging
 import os
 import pathlib
-import subprocess
 import sys
 
-from tempfile import NamedTemporaryFile
-from typing import List, Optional, Union
+from typing import List, Optional, Sequence, Union
 
-from airflow.exceptions import AirflowException, AirflowSkipException
-from airflow.hooks.S3_hook import S3Hook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
+from airflow.providers.amazon.aws.operators.s3 import S3FileTransformOperator
 
 
-class LoopS3FileTransformOperator(BaseOperator):
+class LoopS3FileTransformOperator(S3FileTransformOperator, BaseOperator):
     """
-    Copies data from a source S3 location to a temporary location on the
-    local filesystem. Runs a transformation on this file as specified by
-    the transformation script and uploads the output to a destination S3
-    location.
-
-    The locations of the source and the destination files in the local
-    filesystem is provided as first and second arguments to the
-    transformation script. The transformation script is expected to read the
-    data from source, transform it and write the output to the local
-    destination file. The operator then takes over control and uploads the
-    local destination file to S3.
-
-    S3 Select is also available to filter the source contents. Users can
-    omit the transformation script if S3 Select expression is specified.
-
-    :param source_s3_keys: The list of keys to be retrieved from S3. (templated)
-    :type source_s3_key: list
-    :param source_aws_conn_id: source s3 connection
-    :type source_aws_conn_id: str
-    :param source_verify: Whether or not to verify SSL certificates for S3 connection.
-        By default SSL certificates are verified.
-        You can provide the following values:
-
-        - ``False``: do not validate SSL certificates. SSL will still be used
-             (unless use_ssl is False), but SSL certificates will not be
-             verified.
-        - ``path/to/cert/bundle.pem``: A filename of the CA cert bundle to uses.
-             You can specify this argument if you want to use a different
-             CA cert bundle than the one used by botocore.
-
-        This is also applicable to ``dest_verify``.
-    :type source_verify: bool or str
-    :param dest_aws_conn_id: destination s3 connection
-    :type dest_aws_conn_id: str
-    :param replace: Replace dest S3 key if it already exists
-    :type replace: bool
-    :param transform_script: location of the executable transformation script
-    :type transform_script: str
-    :param select_expression: S3 Select expression
-    :type select_expression: str
+    This operator extends Airflow's built-in S3FileTransformOperator to iterate over multiple files.
+    In addition, the new `dest_s3_file_extension` argument provides greater transparency in output type.
     """
+
     template_fields = ('source_s3_keys', 'dest_s3_prefix', 'transform_script')
 
-    @apply_defaults
     def __init__(self,
         source_s3_keys: Optional[List[str]] = None,
         dest_s3_prefix: Optional[str] = None,
         dest_s3_file_extension: Optional[str] = None,
 
         *,
-        transform_script: Optional[str] = None,
         select_expression: Optional[str] = None,
+        transform_script: Optional[str] = None,
+        script_args: Optional[Sequence[str]] = None,
 
         source_aws_conn_id: str = 'aws_default',
         source_verify: Optional[Union[bool, str]] = None,
@@ -78,7 +36,8 @@ class LoopS3FileTransformOperator(BaseOperator):
 
         **kwargs
     ):
-        super(LoopS3FileTransformOperator, self).__init__(**kwargs)
+        # We need S3FileTransformOperator's execute, but cannot use its init due to differing datatypes.
+        super(BaseOperator, self).__init__(**kwargs)
 
         self.source_aws_conn_id = source_aws_conn_id
         self.source_s3_keys = source_s3_keys
@@ -89,113 +48,38 @@ class LoopS3FileTransformOperator(BaseOperator):
         self.dest_s3_file_extension = dest_s3_file_extension
         self.dest_verify = dest_verify
 
-        self.transform_script = transform_script
         self.select_expression = select_expression
+        self.transform_script = transform_script
+        self.script_args = script_args or []
 
         self.replace = replace
         self.output_encoding = sys.getdefaultencoding()
 
 
     def execute(self, context):
+        """
+        Loop over source and destination keys, using the S3FileTransformOperator's execute for each.
 
-        ###
-        if not self.transform_script and not self.select_expression:
-            raise AirflowException(
-                "Either `transform_script` or `select_expression` must be specified"
-            )
-
-        if not self.source_s3_keys:
-            raise AirflowSkipException(
-                "No files found in source S3 bucket to transfer"
-            )
-
-        ###
-        source_s3 = S3Hook(
-            aws_conn_id=self.source_aws_conn_id,
-            verify=self.source_verify
-        )
-        dest_s3 = S3Hook(
-            aws_conn_id=self.dest_aws_conn_id,
-            verify=self.dest_verify
-        )
-
-        ###
+        Technically, monkey-patching class attributes in execute is a coding faux pas, but it allows us to utilize
+        S3FileTransformOperator's execute as is without rebuilding it here from scratch.
+        """
         transferred_keys = []
 
         for source_s3_key in self.source_s3_keys:
-            logging.info(f"Downloading source S3 file: {source_s3_key}")
 
-            #
-            if not source_s3.check_for_key(source_s3_key):
-                logging.warning(f"{source_s3_key} does not exist")
-                continue
-            elif source_s3_key.endswith('/'):
-                self.log.info(f"{source_s3_key} is a directory, ignoring transfer")
-                continue
-            source_s3_key_object = source_s3.get_key(source_s3_key)
+            # Monkey-patch the missing `source_s3_key` and `dest_s3_key` arguments used in super's execute.
+            self.source_s3_key = source_s3_key
 
-            #
-            with NamedTemporaryFile("wb") as f_source, NamedTemporaryFile("wb") as f_dest:
-                logging.info(
-                    f"Dumping S3 file {source_s3_key} contents to local file {f_source.name}"
-                )
+            source_file_extension = pathlib.Path(self.source_s3_key).suffix
+            if not self.dest_s3_file_extension:
+                self.dest_s3_file_extension = source_file_extension
 
-                if self.select_expression:
-                    content = source_s3.select_key(
-                        key=source_s3_key,
-                        expression=self.select_expression
-                    )
-                    f_source.write(content.encode("utf-8"))
-                else:
-                    source_s3_key_object.download_fileobj(Fileobj=f_source)
+            self.dest_s3_key = os.path.join(
+                self.dest_s3_prefix,
+                source_s3_key.replace(source_file_extension, self.dest_s3_file_extension).split('/')[-1]
+            )
 
-                f_source.flush()  #
+            super(S3FileTransformOperator).execute(context)
+            transferred_keys.append(self.dest_s3_key)
 
-                ###
-                if self.transform_script:
-                    process = subprocess.Popen(
-                        [self.transform_script, f_source.name, f_dest.name],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT,
-                        close_fds=True
-                    )
-
-                    logging.info("Output:")
-                    for line in iter(process.stdout.readline, b''):
-                        logging.info(line.decode(self.output_encoding).rstrip())
-
-                    process.wait()  #
-
-                    if process.returncode > 0:
-                        raise AirflowException(
-                            f"Transform script failed: {process.returncode}"
-                        )
-                    else:
-                        logging.info(
-                            f"Transform script successful. Output temporarily located at {f_dest.name}"
-                        )
-
-                ###
-                logging.info("Uploading transformed file to S3")
-
-                source_file_extension = pathlib.Path(source_s3_key).suffix
-
-                if not self.dest_s3_file_extension:
-                    self.dest_s3_file_extension = source_file_extension
-
-                dest_s3_key = os.path.join(
-                    self.dest_s3_prefix,
-                    source_s3_key.replace(source_file_extension, self.dest_s3_file_extension).split('/')[-1]
-                )
-                f_dest.flush()  #
-
-                dest_s3.load_file(
-                    filename=f_dest.name,
-                    key=dest_s3_key,
-                    replace=self.replace
-                )
-
-                logging.info("Upload successful")
-                transferred_keys.append(dest_s3_key)
-        
         return transferred_keys
