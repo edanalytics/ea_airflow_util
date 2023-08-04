0cc04fa32d2f56bd38d1df346a463a8adf551965
jkaiser <jayckaiser@gmail.com>
2023-08-04 16:40:13 -0500

    Refactor s3_import (untested)

--- ea_airflow_util/dags/dag_util/s3_to_postgres.py
+++ ea_airflow_util/dags/dag_util/s3_to_postgres.py
@@ -1,72 +1,79 @@
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 import logging
+
 from airflow.exceptions import AirflowSkipException, AirflowException
-from airflow.hooks.S3_hook import S3Hook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.hooks.sql import fetch_one_handler
+
 
 def s3_import(
-        pg_conn_id,
-        s3_conn_id,
-        dest_table,
-        column_customization,
-        options,
-        s3_key,
-        s3_region,
-        truncate=False,
-        delete_qry=None,
-        metadata_qry=None,
-        **context
+    pg_conn_id: str,
+    s3_conn_id: str,
+
+    dest_table: str,
+    column_customization: str,
+    options: str,
+
+    s3_key: str,
+    s3_region: str,
+
+    truncate: bool = False,
+    delete_qry: str = None,
+    metadata_qry: str = None,
+
+    **context
 ):
-    if column_customization is None:
-        column_customization = ''
-    if options is None:
-        options = ''
-
-    if s3_key is None:
-        s3_key = context.get('templates_dict').get('s3_key')
-
-    hook = PostgresHook(pg_conn_id)
-    conn = hook.get_conn()
+    column_customization = column_customization or ''
+    options = options or ''
+    s3_key = s3_key or context['templates_dict'].get('s3_key')
 
     # temporary: pull and use creds until IAM role set up
     s3_hook = S3Hook(s3_conn_id)
     s3_creds = s3_hook.get_connection(s3_hook.aws_conn_id)
-    s3_bucket = s3_creds.schema
 
     # make sure content is > 0 bytes, if not, skip this
+    s3_bucket = s3_creds.schema
     if (s3_hook.get_key(key=s3_key, bucket_name=s3_bucket).get()['ContentLength']) == 0:
-        print("File at this s3 key is empty.")
+        logging.info("File at this s3 key is empty.")
         raise AirflowSkipException
 
-    if truncate and not delete_qry:
-        with conn.cursor() as cur:
-            logging.info('Truncating table')
-            cur.execute(f'truncate table {dest_table};')
-    elif not truncate and delete_qry:
-        with conn.cursor() as cur:
-            logging.info('Deleting from table')
-            cur.execute(delete_qry)  
-    elif truncate and delete_qry:
+    # Make sure only one data-reset query is provided.
+    if truncate and delete_qry:
         raise ValueError('Only specify one of truncate, delete_qry')
 
+    # Build optional delete/truncate query followed by insert.
+    conn = PostgresHook(pg_conn_id).get_conn()
+    queries_to_run = []
+
+    if truncate:
+        logging.info('Truncating table')
+        queries_to_run.append(f"truncate table {dest_table};")
+
+    if delete_qry:
+        logging.info('Deleting from table')
+        queries_to_run.append(delete_qry)
+
     copy_qry = f"""
-    select aws_s3.table_import_from_s3(
-    '{dest_table}',
-    '{column_customization}',
-    '{options}',
-    aws_commons.create_s3_uri('{s3_bucket}', '{s3_key}', '{s3_region}'),
-    aws_commons.create_aws_credentials('{s3_creds.login}', '{s3_creds.password}', '')
-    );
+        select aws_s3.table_import_from_s3(
+            '{dest_table}',
+            '{column_customization}',
+            '{options}',
+            aws_commons.create_s3_uri('{s3_bucket}', '{s3_key}', '{s3_region}'),
+            aws_commons.create_aws_credentials('{s3_creds.login}', '{s3_creds.password}', '')
+        );
     """
+    queries_to_run.append(copy_qry)
 
     logging.info('Beginning insert')
-    with conn.cursor() as cur:
-        cur.execute(copy_qry)
-        ret_value = cur.fetchone()
+    ret_value = conn.run(
+        sql=queries_to_run,
+        autocommit=False,
+        handler=fetch_one_handler,
+        return_last=True
+    )
 
+    # Run optional metadata query before returning the fetched insert output.
     if metadata_qry:
-        with conn.cursor() as cur:
-            cur.execute(metadata_qry.format(s3_key=s3_key))
-    conn.commit()
+        conn.run(metadata_qry.format(s3_key=s3_key))
 
     logging.info(ret_value)
