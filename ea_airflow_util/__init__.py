33aaa0964ccb27bde4dfbed9bcecf731ea633d5b
Jay Kaiser <jayckaiser@gmail.com>
2023-08-25 12:47:42 -0500

    Update __init__.py
    Add SFTPToSnowflakeDag to init.

--- ea_airflow_util/__init__.py
+++ ea_airflow_util/__init__.py
@@ -2,6 +2,7 @@ from ea_airflow_util.dags.aws_param_store_to_airflow_dag import AWSParamStoreToA
 from ea_airflow_util.dags.run_dbt_airflow_dag import RunDbtDag
 from ea_airflow_util.dags.update_dbt_docs_dag import UpdateDbtDocsDag
 from ea_airflow_util.dags.dbt_snapshot_dag import DbtSnapshotDag
+from ea_airflow_util.dags.sftp_to_snowflake_dag import SFTPToSnowflakeDag
 
 import ea_airflow_util.dags.dag_util.slack_callbacks as slack_callbacks
 from ea_airflow_util.dags.callables.variable import check_variable, update_variable
