2fbfc8ca22811d91e4917c25fe70572772856570
ejoranlienea <ejoranlien@edanalytics.org>
2023-11-15 10:39:56 -0600

    Feature/s3 dir to postgres (#32)
    * add dir to postgres, rename
    
    * bump version

--- ea_airflow_util/dags/dag_util/s3_to_postgres.py
+++ ea_airflow_util/dags/dag_util/s3_to_postgres.py
@@ -4,7 +4,24 @@ from airflow.exceptions import AirflowSkipException, AirflowException
 from airflow.hooks.S3_hook import S3Hook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
-def s3_import(
+def _list_s3_keys(
+        s3_hook: S3Hook,
+        s3_bucket: str,
+        s3_key: str,
+):
+    """
+    List the keys at a specified S3 location, then filter out directories.
+    """
+    subkeys = s3_hook.list_keys(s3_bucket, s3_key)
+
+    if subkeys is None or len(subkeys) == 0:
+        raise AirflowSkipException
+
+    # Remove directories and return.
+    return [subkey for subkey in subkeys if not subkey.endswith("/")]
+
+
+def s3_to_postgres(
         pg_conn_id,
         s3_conn_id,
         dest_table,
@@ -70,3 +87,56 @@ def s3_import(
     conn.commit()
 
     logging.info(ret_value)
+
+def s3_dir_to_postgres(
+        pg_conn_id,
+        s3_conn_id,
+        dest_table,
+        column_customization,
+        options,
+        s3_key,
+        s3_region,
+        truncate=False,
+        delete_s3_dir=False,
+        metadata_qry=None,
+        **context
+):
+    s3_hook = S3Hook(s3_conn_id)
+    s3_creds = s3_hook.get_connection(s3_hook.aws_conn_id)
+    s3_bucket = s3_creds.schema
+    s3_keys = _list_s3_keys(s3_conn_id, s3_bucket, s3_key)
+
+    if truncate:
+        hook = PostgresHook(pg_conn_id)
+        conn = hook.get_conn()
+        with conn.cursor() as cur:
+            logging.info('Truncating table')
+            cur.execute(f'truncate table {dest_table};')
+            conn.commit()
+
+    failed_count = 0
+    for key in s3_keys:
+        logging.info(f'Loading key: {key}')
+        try:
+            s3_to_postgres(pg_conn_id=pg_conn_id,
+                           s3_conn_id=s3_conn_id,
+                           dest_table=dest_table,
+                           column_customization=column_customization,
+                           options=options,
+                           s3_key=key,
+                           s3_region=s3_region,
+                           truncate=False,
+                           delete_qry=None,
+                           metadata_qry=metadata_qry)
+        except Exception as e:
+            logging.error(e)
+            failed_count += 1
+            continue
+
+    if failed_count == len(s3_keys):
+        raise AirflowException('All inserts failed')
+    else:
+        logging.info(f'Loaded {len(s3_keys) - failed_count} of {len(s3_keys)} keys.')
+    
+    if delete_s3_dir:
+        s3_hook.delete_objects(s3_bucket, s3_keys)
