6c84ccfaa42446f57001cace8e53522b46196f22
jayckaiser <jayckaiser@gmail.com>
2023-12-01 09:27:35 -0600

    Refactor project to move Python callables into their own location; add deprecation warnings when referenced from the old path.
    TODO: Test this.

--- ea_airflow_util/dags/dag_util/snowflake_to_disk.py
+++ ea_airflow_util/dags/dag_util/snowflake_to_disk.py
@@ -1,34 +1,9 @@
-from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
-import csv
+import warnings
 
-def snowflake_to_disk(
-        snowflake_conn_id,
-        query,
-        local_path,
-        sep=',',
-        quote_char='"',
-        chunk_size=1000,
-        **context
-        ):
-    hook = SnowflakeHook(snowflake_conn_id)
-    conn = hook.get_conn()
+warnings.warn(
+    "The 'ea_airflow_util.dags.callables.dag_util.snowflake_to_disk' module is deprecated and will be removed in a future update."
+    "Please update your code to use the new location: 'ea_airflow_util.callables.snowflake'",
+    DeprecationWarning
+)
 
-    with open(local_path, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=sep, 
-                                quotechar=quote_char, quoting=csv.QUOTE_MINIMAL)
-        # fetch and write header
-        meta = conn.cursor().describe(query)
-        header = [x[0].lower() for x in meta]
-        csv_writer.writerow(header)
-
-        # run query, chunked
-        cur = conn.cursor().execute(query)
-        while True:
-            res = cur.fetchmany(chunk_size)
-            if len(res) == 0:
-                break
-            for row in res:
-                csv_writer.writerow(row)
-    conn.close()
-
-    return(local_path)
+from ea_airflow_util.callables.snowflake import snowflake_to_disk
