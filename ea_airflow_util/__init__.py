718f1276084e1800540c8f4a9d82d4aaf2f6c855
jayckaiser <jayckaiser@gmail.com>
2024-01-04 19:34:15 -0600

    Use lazy approach for rerouting deprecated imports (untested).

--- ea_airflow_util/__init__.py
+++ ea_airflow_util/__init__.py
@@ -1,5 +1,3 @@
-import sys
-
 from ea_airflow_util.dags.aws_param_store_to_airflow_dag import AWSParamStoreToAirflowDAG
 from ea_airflow_util.dags.run_dbt_airflow_dag import RunDbtDag
 from ea_airflow_util.dags.update_dbt_docs_dag import UpdateDbtDocsDag
@@ -12,26 +10,30 @@ from ea_airflow_util.callables.variable import check_variable, update_variable
 
 # Reroute deprecated module pathing.
 # Using this SO as inspiration: https://stackoverflow.com/a/72244240
-from ea_airflow_util.callables import airflow as airflow_callables
-sys.modules['ea_airflow_util.dags.callables.xcom_util'] = airflow_callables
+import importlib
+import sys
+from types import ModuleType
 
-from ea_airflow_util.callables import s3 as s3_callables
-sys.modules['ea_airflow_util.dags.callables.dag_util.s3_to_postgres'] = s3_callables
+class LazyModule(ModuleType):
+    def __init__(self, name, mod_name):
+        super().__init__(name)
+        self.__mod_name = mod_name
 
-from ea_airflow_util.callables import slack as slack_callbacks
-sys.modules['ea_airflow_util.dags.dag_util.slack_callbacks'] = slack_callbacks
+    def __getattr__(self, attr):
+        if "_lazy_module" not in self.__dict__:
+            self._lazy_module = importlib.import_module(self.__mod_name, package="ea_airflow_util")
+        return self._lazy_module.__getattr__(attr)
 
-from ea_airflow_util.callables import snowflake as snowflake_callables
-sys.modules['ea_airflow_util.dags.dag_util.snowflake_to_disk'] = snowflake_callables
+rename_mapping = {
+    "ea_airflow_util.dags.dag_util.xcom_util"          : "ea_airflow_util.callables.airflow",
+    "ea_airflow_util.dags.dag_util.s3_to_postgres"     : "ea_airflow_util.callables.s3",
+    "ea_airflow_util.dags.dag_util.slack_callbacks"    : "ea_airflow_util.callables.slack",
+    "ea_airflow_util.dags.dag_util.snowflake_to_disk"  : "ea_airflow_util.callables.snowflake",
+    "ea_airflow_util.dags.dag_util.ssm_parameter_store": "ea_airflow_util.callables.ssm",
+    "ea_airflow_util.dags.callables.variable"          : "ea_airflow_util.callables.variable",
+    "ea_airflow_util.dags.operators.dbt_operators"     : "ea_airflow_util.providers.dbt.operators.dbt",
+    "ea_airflow_util.dags.operators.loop_s3_file_transform_operator": "ea_airflow_util.providers.aws.operators.s3",
+}
 
-from ea_airflow_util.callables import ssm as ssm_callables
-sys.modules['ea_airflow_util.dags.dag_util.ssm_parameter_store'] = ssm_callables
-
-from ea_airflow_util.callables import variable as variable_callables
-sys.modules['ea_airflow_util.dags.callables.variable'] = variable_callables
-
-from ea_airflow_util.providers.dbt.operators import dbt as dbt_operators
-sys.modules['ea_airflow_util.dags.operators.dbt_operators'] = dbt_operators
-
-from ea_airflow_util.providers.aws.operators import s3 as s3_operators
-sys.modules['ea_airflow_util.dags.operators.loop_s3_file_transform_operator'] = s3_operators
+for old_path, new_path in rename_mapping.items():
+    sys.modules[old_path] = LazyModule(old_path, new_path)
