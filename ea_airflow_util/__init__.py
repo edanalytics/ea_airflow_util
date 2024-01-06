8c7c92aa7691624e45b6d85a9597b8cc2a904130
jayckaiser <jayckaiser@gmail.com>
2024-01-06 17:29:49 -0600

    Attempt refactor of deprecated imports mapping.

--- ea_airflow_util/__init__.py
+++ ea_airflow_util/__init__.py
@@ -13,27 +13,56 @@ from ea_airflow_util.callables.variable import check_variable, update_variable
 import importlib
 import sys
 from types import ModuleType
+from typing import Optional
 
 class LazyModule(ModuleType):
-    def __init__(self, name, mod_name):
+    def __init__(self, name: str, mod_name: str, child_mapping: Optional[dict] = None):
         super().__init__(name)
         self.__mod_name = mod_name
+        self.__child_mapping = child_mapping or {}
 
     def __getattr__(self, attr):
         if "_lazy_module" not in self.__dict__:
             self._lazy_module = importlib.import_module(self.__mod_name, package="ea_airflow_util")
-        return self._lazy_module.__getattr__(attr)
+
+        if attr in self.__child_mapping:
+            return getattr(self._lazy_module, self.__child_mapping[attr])
+        else:
+            return getattr(self._lazy_module, attr)
+
 
 rename_mapping = {
-    "ea_airflow_util.dags.dag_util.xcom_util"          : "ea_airflow_util.callables.airflow",
-    "ea_airflow_util.dags.dag_util.s3_to_postgres"     : "ea_airflow_util.callables.s3",
-    "ea_airflow_util.dags.dag_util.slack_callbacks"    : "ea_airflow_util.callables.slack",
-    "ea_airflow_util.dags.dag_util.snowflake_to_disk"  : "ea_airflow_util.callables.snowflake",
-    "ea_airflow_util.dags.dag_util.ssm_parameter_store": "ea_airflow_util.callables.ssm",
-    "ea_airflow_util.dags.callables.variable"          : "ea_airflow_util.callables.variable",
-    "ea_airflow_util.dags.operators.dbt_operators"     : "ea_airflow_util.providers.dbt.operators.dbt",
-    "ea_airflow_util.dags.operators.loop_s3_file_transform_operator": "ea_airflow_util.providers.aws.operators.s3",
+    "ea_airflow_util.dags.dag_util": {
+        "name": "ea_airflow_util.callables",
+        "child_mapping": {
+            "xcom_util": "airflow",
+            "s3_to_postgres": "s3",
+            "slack_callbacks": "slack",
+            "snowflake_to_disk": "snowflake",
+            "ssm_parameter_store": "ssm",
+        }
+    },
+    "ea_airflow_util.dags.callables": {
+        "name": "ea_airflow_util.callables",
+        "child_mapping": {
+            "variable", "variable"
+        }
+    },
+    "ea_airflow_util.dags.operators": {
+        "name": "ea_airflow_util.providers.dbt.operators.dbt",
+        "child_mapping": {
+            "dbt_operators": "dbt.operators.dbt",
+            "loop_s3_file_transform_operator": "aws.operators.s3"
+        }
+    },
 }
 
-for old_path, new_path in rename_mapping.items():
-    sys.modules[old_path] = LazyModule(old_path, new_path)
+for old_path, new_path_metadata in rename_mapping.items():
+    new_path = new_path_metadata['name']
+    child_mapping = new_path_metadata['child_mapping']
+    sys.modules[old_path] = LazyModule(old_path, new_path, child_mapping=child_mapping)
+
+    for old_child, new_child in child_mapping.items():
+        full_old_path = '.'.join([old_path, old_child])
+        full_new_path = '.'.join([new_path, new_child])
+        sys.modules[full_old_path] = LazyModule(full_old_path, full_new_path)
