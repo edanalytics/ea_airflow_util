ec6fa86206580e55b2c4a45f3819d04ce8088b53
jayckaiser <jayckaiser@gmail.com>
2024-02-07 13:17:02 -0600

    Add missing package calls in init.

--- ea_airflow_util/__init__.py
+++ ea_airflow_util/__init__.py
@@ -5,6 +5,8 @@ from ea_airflow_util.dags.s3_to_snowflake_dag import S3ToSnowflakeDag
 from ea_airflow_util.dags.dbt_snapshot_dag import DbtSnapshotDag
 from ea_airflow_util.dags.sftp_to_snowflake_dag import SFTPToSnowflakeDag
 
+from ea_airflow_util.callables.airflow import xcom_pull_template
+from ea_airflow_util.callables import slack as slack_callbacks
 from ea_airflow_util.callables.variable import check_variable, update_variable
 
 
