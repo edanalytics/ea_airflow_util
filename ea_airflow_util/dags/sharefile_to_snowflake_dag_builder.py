8ce7eb99434b51dfb53b6f46f5cc922c34330116
mberrien-fitzsimons <mberrien@edanalytics.org>
2025-03-07 11:32:19 -0700

    updated variable inputs to init and individual methods

--- ea_airflow_util/dags/sharefile_to_snowflake_dag_builder.py
+++ ea_airflow_util/dags/sharefile_to_snowflake_dag_builder.py
@@ -16,32 +16,18 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 class SharefileTransferToSnowflakeDagBuilder:
     """
-    :param configs_dir:
-    :param airflow_config:
-    :param sharefile_sources:
-    :param sharefile_conn_id:
-    :param s3_destination_key:
-    :param s3_conn_id:
-    :param snowflake_conn_id:
-    :: 
+    ::
     """
-    def __init__(self, 
-                 configs_dir: str, 
-                 airflow_config: str, 
-                 sharefile_sources: str,
-                 sharefile_conn_id: str,
-                 s3_destination_key: str,
-                 s3_conn_id: str,
-                 snowflake_conn_id: str
+    def __init__(self,
+                dag_id,
+                airflow_default_args, 
+                file_sources, 
+                schedule_interval = None
     ):
-        self.configs_dir = configs_dir
-        self.airflow_config = airflow_config
-        self.dag_configs = self.airflow_configs['sharefile_transfer_pred']
-        self.file_sources_dict = io_helpers.safe_load_yaml(configs_dir, sharefile_sources)
-        self.sharefile_conn_id = sharefile_conn_id
-        self.s3_destination_key = s3_destination_key
-        self.s3_conn_id = s3_conn_id
-        self.snowflake_conn_id = snowflake_conn_id
+        self.dag_id = dag_id
+        self.airflow_default_args = airflow_default_args
+        self.file_sources = file_sources
+        self.schedule_interval = schedule_interval
 
         self.dag = None
         self._initialize_dag()
@@ -49,19 +35,18 @@ class SharefileTransferToSnowflakeDagBuilder:
     def _initialize_dag(self):
         params = {
             "file_sources": Param(
-                default=list(self.file_sources_dict.get(self.run_type, {})),
-                examples=list(self.file_sources_dict.get(self.run_type, {})),
-                type="array",
-                description="Newline-separated list of file sources to pull from ShareFile (default all in `sharefile_sources.yml`)",
+                default=list(self.file_sources, {}),
+                examples=list(self.file_sources, {}),
+                type="list",
+                description="Newline-separated list of file sources to pull from ShareFile",
             ),
         }
 
         self.dag = DAG(
-            dag_id=f"sharefile_{self.run_type}_transfer",
-            default_args=self.dag_configs['default_args'],
-            schedule_interval=self.dag_configs['schedule_interval'],
+            dag_id=self.dag_id,
+            default_args=self.airflow_default_args,
+            schedule_interval=self.schedule_interval,
             params=params,
-            user_defined_macros={'slack_conn_id': None},
             catchup=False,
         )
 
@@ -77,56 +62,58 @@ class SharefileTransferToSnowflakeDagBuilder:
             dag=self.dag
         )
 
-    # def transfer_sharefile_to_disk(self, file, file_configs):
-    #     """Transfers file from ShareFile to local disk."""
-    #     return SharefileToDiskOperator(
-    #         task_id=f"transfer_{file}_to_disk",
-    #         sharefile_conn_id=self.dag_configs['sharefile_conn_id'],
-    #         sharefile_path=file_configs['sharefile_path'],
-    #         local_path=self._generate_file_path(file, 'disk'),
-    #         delete_remote=self.dag_configs['delete_remote'],
-    #         dag=self.dag
-    #     )
+    def transfer_sharefile_to_disk(self, file, sharefile_conn_id, 
+                                    sharefile_path, local_path, delete_remote):
+        """Transfers file from ShareFile to local disk."""
+        return SharefileToDiskOperator(
+            task_id=f"transfer_{file}_to_disk",
+            sharefile_conn_id=sharefile_conn_id,
+            sharefile_path=sharefile_path,
+            local_path=local_path,
+            delete_remote=delete_remote,
+            dag=self.dag
+        )
 
-    # def transform_to_jsonl(self, transfer_task, file):
-    #     """Transforms CSV to JSONL."""
-    #     return PythonOperator(
-    #         task_id=f"transform_{file}_to_jsonl",
-    #         python_callable=jsonl.translate_csv_file_to_jsonl,
-    #         op_kwargs={
-    #             'local_path': xcom_pull_template(transfer_task),
-    #             'output_path': None,
-    #             'delete_csv': True,
-    #             'metadata_dict': {'file_source': file},
-    #         },
-    #         dag=self.dag
-    #     )
+    def transform_to_jsonl(self, file, local_path, delete_csv):
+        """Transforms CSV to JSONL."""
+        return PythonOperator(
+            task_id=f"transform_{file}_to_jsonl",
+            python_callable=jsonl.translate_csv_file_to_jsonl,
+            op_kwargs={
+                'local_path': local_path,
+                'output_path': None,
+                'delete_csv': delete_csv,
+                'metadata_dict': {'file_source': file},
+            },
+            dag=self.dag
+        )
 
-    # def _transfer_disk_to_s3(self, file):
-    #     """Transfers file from local disk to S3."""
-    #     return PythonOperator(
-    #         task_id=f"transfer_{file}_to_s3",
-    #         python_callable=s3.local_filepath_to_s3,
-    #         op_kwargs={
-    #             'local_filepath': self._generate_file_path(file, 'disk'),
-    #             's3_destination_key': f"ea_research/{file}",
-    #             's3_conn_id': self.dag_configs['s3_conn_id']
-    #         },
-    #         dag=self.dag
-    #     )
+    def transfer_disk_to_s3(self, file):
+        """Transfers file from local disk to S3."""
+        return PythonOperator(
+            task_id=f"transfer_{file}_to_s3",
+            python_callable=s3.local_filepath_to_s3,
+            op_kwargs={
+                'local_filepath': self._generate_file_path(file, 'disk'),
+                's3_destination_key': f"ea_research/{file}",
+                's3_conn_id': self.dag_configs['s3_conn_id']
+            },
+            dag=self.dag
+        )
 
-    # def transfer_s3_to_snowflake(self, file):
-    #     """Transfers file from S3 to Snowflake."""
-    #     return S3ToSnowflakeOperator(
-    #         task_id=f"{file}_s3_to_snowflake",
-    #         snowflake_conn_id=self.dag_configs['snowflake_conn_id'],
-    #         database=self.dag_configs['snowflake_db'],
-    #         schema=self.dag_configs['snowflake_schema'],
-    #         table_name=file,
-    #         s3_destination_key=f"ea_research/{file}",
-    #         full_refresh=True,
-    #         dag=self.dag
-    #     )
+    def transfer_s3_to_snowflake(self, file, snowflake_conn_id, database, schema,
+                                 full_refresh):
+        """Transfers file from S3 to Snowflake."""
+        return S3ToSnowflakeOperator(
+            task_id=f"{file}_s3_to_snowflake",
+            snowflake_conn_id=snowflake_conn_id,
+            database=database,
+            schema=schema,
+            table_name=file,
+            s3_destination_key=f"ea_research/{file}",
+            full_refresh=full_refresh,
+            dag=self.dag
+        )
 
     # def build_dag(self):
     #     """Builds the complete DAG, setting up tasks and their dependencies."""
