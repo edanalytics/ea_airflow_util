be2907201f595c83b122c8cdf4c79bc720364a4b
mberrien-fitzsimons <mberrien@edanalytics.org>
2025-09-04 11:20:43 -0500

    added updated file so that I can see what changes were made

--- ea_airflow_util/callables/jsonl.py
+++ ea_airflow_util/callables/jsonl.py
@@ -1,14 +1,21 @@
-import csv
-import json
-import logging
-import os
+#!/usr/bin/env python3
 
-from typing import Iterator, Optional
+'''
+Note: You'll need to make this file executable in whatever environment you use it in. Run this in bash 
+`chmod +x /home/airflow/airflow/dags/util/jsonl_executable.py`
+'''
+
+
+import sys
+import os
+import csv
+import logging
+import json
 
 from airflow.exceptions import AirflowException, AirflowSkipException
+from typing import Optional, Iterator
 
 from ea_airflow_util.callables import casing
-
 
 def serialize_json_records_to_disk(
     json_records : Iterator[dict],
@@ -31,6 +38,10 @@ def serialize_json_records_to_disk(
     total_records = 0
 
     with open(output_path, mode) as writer:
+        
+        json_records = list(json_records)
+        logging.info(f"Number of records parsed from CSV: {len(json_records)}")
+        logging.info(f"First record: {json_records[0] if json_records else 'None'}")
 
         for json_record in json_records:
 
@@ -44,6 +55,7 @@ def serialize_json_records_to_disk(
 
             writer.write( json.dumps(json_record) + '\n' )
             total_records += 1
+        logging.info(f"Current Record: {total_records}")
 
     if total_records == 0:
         raise AirflowSkipException(
@@ -56,62 +68,44 @@ def serialize_json_records_to_disk(
 
 def translate_csv_file_to_jsonl(
     local_path: str,
-    output_path: str = None,
-    delete_csv : bool = False,
+    output_path: str,
+    delete_csv: bool = False,
     metadata_dict: Optional[dict] = None,
     to_snake_case: bool = False,
     **kwargs
-):
-    """
-    Main for transforming CSV data to JSON lines (for consistency downstream).
+):  
+    logging.info(f"Local Path Used: {local_path}")
+    logging.info(f"Output Path Used: {output_path}")
 
-    Records are collected from the CSV as a generator and written one-by-one to the output file.
-    Optional **kwargs for `serialize_json_records_to_disk` can be specified.
-
-    If `delete_csv is True`, delete the CSV source path.
-    """
-
-    # note: csv path was initially intended to be a full path to a file
-    # it can now be a directory in case there are multiple files
-
-    # error if csv path is a directory but output path is not null (the loop will overwrite)
     if os.path.isdir(local_path) and output_path is not None:
-        raise AirflowException(
-            f"Trying to write multiple files to one output file. You will overwrite your output"
-        )
+        raise AirflowException("Trying to write multiple files to one output file.")
 
-    # loop and find all the files
-    for root, _, files in os.walk(local_path):
-        # loop over the files in this directory (since there could potentially be multiple)
-        for file in files:
-
-            # create full local name
-            full_local_path = os.path.join(root, file)
-
-            # create output path
-            if output_path is None:
-                output_path_new = full_local_path.replace('.csv', '.jsonl')
-            else:
-                output_path_new = output_path
-
-            try:
-                with open(full_local_path, 'r') as reader:
-                    json_records = csv.DictReader(reader)
-                    serialize_json_records_to_disk(json_records, output_path_new, "w", metadata_dict, to_snake_case, **kwargs)
-            except UnicodeDecodeError:
-                with open(full_local_path, 'r', encoding='latin1', errors='backslashreplace') as reader:
-                    json_records = csv.DictReader(reader)
-                    serialize_json_records_to_disk(json_records, output_path_new, "w", metadata_dict, to_snake_case, **kwargs)
-
-            if delete_csv:
-                os.remove(full_local_path)  # TODO: Cannot delete directories (use shutil)
-                logging.info(
-                    f"Local path `{full_local_path}` deleted."
-                )
-
-    # this is slightly awkward bc the output path is being created within the above loop
-    # but technically if the csv path ends with a file and is not a directory, there will only be one output path anyway
-    if local_path.endswith('.csv'):
-        return output_path
+    if os.path.isfile(local_path):
+        files = [os.path.basename(local_path)]
+        root = os.path.dirname(local_path)
+        logging.info(f"root: {root}, files: {files}")
     else:
-        return local_path
+        for root, _, files in os.walk(local_path):
+            logging.info(f"root: {root}, files: {files}")
+    for file in files:
+        full_local_path = os.path.join(root, file)
+        output_path_new = output_path or full_local_path.replace('.csv', '.jsonl')
+        logging.info(f"Full Local Path within for loop: {full_local_path}")
+
+        try:
+            with open(full_local_path, 'r') as reader:
+                json_records = csv.DictReader(reader)
+                serialize_json_records_to_disk(json_records, output_path_new, "w", metadata_dict, to_snake_case, **kwargs)
+        except UnicodeDecodeError:
+            with open(full_local_path, 'r', encoding='latin1', errors='backslashreplace') as reader:
+                json_records = csv.DictReader(reader)
+                serialize_json_records_to_disk(json_records, output_path_new, "w", metadata_dict, to_snake_case, **kwargs)
+
+        if delete_csv:
+            os.remove(full_local_path)
+            logging.info(f"Deleted {full_local_path}")
+
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+    translate_csv_file_to_jsonl(input_path, output_path)
