04bddbf7dd92f0c25ef44ee2e4c89fbc2e532005
jayckaiser <jayckaiser@gmail.com>
2026-07-08 13:37:30 -0500

    Allow user rotations to be subsetted via DAG Params.

--- ea_airflow_util/dags/snowflake_keypair_dag.py
+++ ea_airflow_util/dags/snowflake_keypair_dag.py
@@ -1,5 +1,7 @@
 from typing import List, Optional
 
+from airflow.exceptions import AirflowSkipException
+from airflow.models.param import Param
 from airflow.operators.python import PythonOperator
 
 from ea_airflow_util.dags.ea_custom_dag import EACustomDAG
@@ -19,14 +21,31 @@ class SnowflakeKeypairRotationDag:
         self.snowflake_users = snowflake_users
         self.key_dir = key_dir
 
-        self.dag = EACustomDAG(**kwargs)
+        params = {
+            "snowflake_users": Param(self.snowflake_users, type="array", description="Subset which users to rotate."),
+        }
+
+        self.dag = EACustomDAG(params=params, **kwargs)
         self.build_snowflake_keypair_rotation_dag()
+
+    def rotate_keypair_wrapper(key_rotator_conn_id: str, snowflake_user: str, output_dir: str, **context):
+        """
+        Simple wrapper to raise a skip exception when the user is not specified in the DAG params. 
+        """
+        if snowflake_user not in context['params']['snowflake_users']:
+            raise AirflowSkipException(f"Snowflake user `{snowflake_user}` was not specified in DAG params for rotation. Skipping...")
+        
+        return snowflake_keypair.rotate_keypair(
+            key_rotator_conn_id=key_rotator_conn_id,
+            snowflake_user=snowflake_user,
+            output_dir=output_dir
+        )
 
     def build_snowflake_keypair_rotation_dag(self, **kwargs) -> None:
         for snowflake_user in self.snowflake_users:
             PythonOperator(
                 task_id=f"rotate_keypair_{snowflake_user}",
-                python_callable=snowflake_keypair.rotate_keypair,
+                python_callable=self.rotate_keypair_wrapper,
                 op_kwargs={
                     "key_rotator_conn_id": self.key_rotator_conn_id,
                     "snowflake_user": snowflake_user,
